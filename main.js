1e5df4b0c7baaba84d5b57ceb98450690c39a121
Jalissa Williams <jalissa_williams@yahoo.com>
2020-11-07 16:25:37 -0500

    adding project page

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 //gsap 
 
-
+const animateLinks = () => {
+    gsap.to('li', {
+        alpha: 1,
+        y: 20,
+        stagger: 0.2,
+    })
+}
 
 const animateLeave = (container) => {
-    return gsap.from('.load-page', { alpha: 1 })
+    console.log('leaving')
+
 }
 
 const animateEnter = (container) => {
-    return gsap.to('.load-page', {
-        alpha: 1,
-        background: '#4C49EC',
-        duration: 2,
-        xPercent: 100
-    })
+    const tl = gsap.timeline()
+    return (
+        tl.to('.load-page', {
+            background: '#4C49EC',
+            width: "100%",
+            left: "0%",
+            duration: 1.2,
+            ease: "Expo.easeInOut"
+        })
+            .to('.load-page', {
+                background: '#4C49EC',
+                width: "100%",
+                left: "100%",
+                duration: 1,
+                delay: 0.3,
+                ease: "Expo.easeInOut"
+            })
+            .set('.load-page', { left: "-100%" })
+            .to('h1', {
+                opacity: 1,
+                y: 20,
+
+            })
+    )
 }
 
 
@@ -23,6 +48,10 @@ barba.init({
         enter(data) {
 
             animateEnter()
+        },
+
+        beforeOnce() {
+            // animateLinks()
         }
     }]
 });
